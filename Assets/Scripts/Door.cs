d40cd484d2e45392520f53cc2f9b6eb75baae7c5
Pene4k <pene4k@mail.ru>
2024-03-11 16:27:28 +0700

    NormalRay+Doors
    123

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     private Animator anim;
+    int i = 0;
+    private bool canOpen= true;
     void Start()
     {
         anim = GetComponent<Animator>();
     }
     void Update()
     {
-        if(PlayerRay.watchDoor == true)
+        if (PlayerRay.watchDoor == true)
+        {
+            if (Input.GetKey(KeyCode.F) && i == 0 && canOpen==true)
             {
                 anim.SetBool("DoorIsOpen", true);
-                print("fasdsgv");
-                if(Input.GetKey(KeyCode.F))
-                    {
-                        print("fasdsgv");
-                        anim.SetBool("DoorIsOpen", true);
-                    }
+                i = 1;
+                canOpen = false;
+                Invoke("WaitPls", 0.2f);
+                Debug.Log("1");
             }
+            if (Input.GetKey(KeyCode.F) && i == 1 && canOpen==true)
+            {
+                anim.SetBool("DoorIsOpen", false);
+                canOpen = false;
+                Invoke("WaitPls", 0.2f);
+                i = 0;
+                Debug.Log("2");
+            }
+
+        }
     }
-    
+    public void WaitPls()
+    {
+        canOpen = true;
+    }
 }
